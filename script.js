5a0ec9a82e92c738f9bcfa6e958ee24728c3f335
dhoni354720 <dhonipatel.official@gmail.com>
2026-04-23 00:17:31 +0530

    safarstudio

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function sayHi() {
-    alert("Welcome to my Website!");
+let journeyMode = false;
+
+function toggleJourney() {
+  journeyMode = !journeyMode;
+
+  document.body.style.background = journeyMode ? "#111" : "#0a0a0a";
+
+  alert(journeyMode 
+    ? "Journey Mode ON: You are seeing our evolution." 
+    : "Journey Mode OFF");
+}
+
+function toggleCard(card) {
+  let text = card.querySelector(".hidden");
+
+  text.style.display = text.style.display === "block" ? "none" : "block";
+}
+
+function contact() {
+  window.location.href = "mailto:safarstudio@gmail.com";
 }
